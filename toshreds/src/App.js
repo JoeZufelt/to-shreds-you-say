81db8dc5c638b8adede42f4305d7f640012bd484
JoeZufelt <joe.zufelt@gmail.com>
2019-04-06 23:58:30 -0400

    app.js updated

--- toshreds/src/App.js
+++ toshreds/src/App.js
@@ -1,25 +1,72 @@
 import React, { Component } from 'react';
-import Game from "./components/Images";
+import Game from "./components/Game";
 import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import Jumbotron from "./components/Jumbotron";
 import Title from "./components/Title";
-import images from "./images.json";
+import Footer from "./components/Footer";
+import Characters from "./images.json";
 
 class App extends Component {
 
   state = {
-    images
+    Characters,
+    clicked: [],
+    score: 0
+  };
+
+  iconClick = event => {
+    const icon = event.target.alt;
+    const clickedIcon = this.state.clicked.indexOf(icon) > -1;
+
+    if (clickedIcon) {
+      this.setState({
+        Characters: this.state.Characters.sort(function () {
+          return 0.5 - Math.random();
+        }),
+        clicked: [],
+        score: 0
+      });
+      alert("Not great. You lost.");
+    } else {
+      this.setState({
+        Characters: this.state.Characters.sort(function () {
+          return 0.5 - Math.random();
+        }),
+        clickedIcon: this.clickedIcon.concat(icon),
+        score: this.state.score + 1
+      },
+        () => {
+          if (this.state.score === 12) {
+            alert("Good news! You win.");
+            this.setState({
+              Characters: this.state.Characters.sort(function () {
+                return 0.5 - Math.random();
+              }),
+              clicked: [],
+              score: 0
+            });
+          }
+        }
+      );
+    }
   };
 
   render() {
     return (
       <Wrapper>
-        <Title>To Shreds You Say...</Title>
-        {this.state.Game.map(images => (
+        <Header
+          score={this.state.score}
+        />
+        <Jumbotron />
+        {this.state.Characters.map(Characters => (
           <Game
-            id = {images.id}
-            image = {images.image}
+          imageClick = {this.imageClick}
+          id = {Characters.id}
+          image = {Characters.image}
           />
         ))}
+        <Footer />
       </Wrapper>
     );
   }
