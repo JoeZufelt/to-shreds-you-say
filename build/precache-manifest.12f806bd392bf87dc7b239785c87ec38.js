2e4d3cae8ec8dd9039e11385882594d3f5e9db33
JoeZufelt <joe.zufelt@gmail.com>
2019-04-09 18:03:05 -0400

    trying to deploy

---
+++ build/precache-manifest.12f806bd392bf87dc7b239785c87ec38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd829f05230f0458f0de",
+    "url": "/to-shreds-you-say/static/css/main.616d4bea.chunk.css"
+  },
+  {
+    "revision": "bd829f05230f0458f0de",
+    "url": "/to-shreds-you-say/static/js/main.19d9b392.chunk.js"
+  },
+  {
+    "revision": "268452b646fedb499b77",
+    "url": "/to-shreds-you-say/static/js/runtime~main.466a995e.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/to-shreds-you-say/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "36b087b2c6c93473aab72c5fb27e403b",
+    "url": "/to-shreds-you-say/index.html"
+  }
+];
