6a27133902ce8ec79fa56fdddd7c798cca33d50e
JoeZufelt <joe.zufelt@gmail.com>
2019-04-09 19:11:57 -0400

    style

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./game.css";
 
 const Game = props => (
-    <div className = "card col-2" onClick = {props.iconClick}>
+    <div className = "card" onClick = {props.iconClick}>
         <div className = "img-container">
             <img alt = {props.id} src = {props.image} />
         </div>
--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./jumbo.css";
 
 const Jumbotron = () => (
-    <div className = "jumbotron">
+    <div className = "jumbotron parallax">
         <h1>Clicky Game!</h1>
         <h2>Click on a Futurama character, but make sure not to click on the same one twice!</h2>
     </div>
